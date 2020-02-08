0e0bed9f29a6b171193f49eaaa3ebc3cd022d620
sergpitk <sergpitk@andi.lv>
2020-02-08 15:07:52 +0200

    bs-portfolio 0.007 RectanglesOutput json

--- src/Controller/RectanglesRestController.php
+++ src/Controller/RectanglesRestController.php
@@ -3,18 +3,37 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RectanglesRestController extends AbstractController
 {
     /**
      * @Route("/generate-rectangles/", name="generate_rectangles", methods={"POST"})
+     * @return JsonResponse
      */
     public function RectanglesInput()
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/RectanglesRestController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/generation-status", name="generation_status", methods={"GET"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function RectanglesOutput(Request $request)
+    {
+        $id = $request->get('id');
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/RectanglesRestController.php',
+            'id'    => $id
         ]);
     }
 }
