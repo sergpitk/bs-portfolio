fe96c853830a183017124b0cb9cf3936da2ae76f
sergpitk <sergpitk@andi.lv>
2020-02-09 15:51:06 +0200

    bs-portfolio 0.027 service RectanglesOverlapValidator

---
+++ src/Service/RectanglesOverlapValidator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sergey Pitkevich <sergpitk@andi.lv>
+ * Date: 09/02/2020
+ * Time: 15:49
+ */
+
+namespace App\Service;
+
+
+class RectanglesOverlapValidator
+{
+
+}
