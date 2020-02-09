443a3f70274c234602e95b499cfbd414f172e2c1
sergpitk <sergpitk@andi.lv>
2020-02-09 15:51:55 +0200

    bs-portfolio 0.028 service RectanglesPngSaver

---
+++ src/Service/RectanglesPngSaver.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sergey Pitkevich <sergpitk@andi.lv>
+ * Date: 09/02/2020
+ * Time: 15:51
+ */
+
+namespace App\Service;
+
+
+class RectanglesPngSaver
+{
+
+}
